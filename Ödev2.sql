534a8fa1bcf6b7dd3f9e449b6aa47ab2d8b35cba
Mustafa Durmuş <59168270+mstfdrms@users.noreply.github.com>
2022-03-26 22:39:12 +0300

    Update Ödev2.sql

--- Ödev2.sql
+++ Ödev2.sql
@@ -1,1 +1,15 @@
+Merhabalar,
 
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız 
+( BETWEEN - AND yapısını kullanınız.)
+Select * from film where replacement_cost between 12.99 and 16.99
+
+2.actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız.
+( IN operatörünü kullanınız.)
+Select * from actor where first_name in ('Penelope','Nick','Ed')
+
+3.film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. 
+( IN operatörünü kullanınız.)
+Select * from film where rental_rate in (0.99,2.99,4.99) and replacement_cost in(12.99,15.99,28.99)
