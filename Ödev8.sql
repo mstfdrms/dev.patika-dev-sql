6643388813182fa234e354d265e2ecbce2264c6d
Mustafa Durmuş <59168270+mstfdrms@users.noreply.github.com>
2022-04-01 22:45:49 +0300

    Update Ödev8.sql

--- Ödev8.sql
+++ Ödev8.sql
@@ -1,1 +1,27 @@
+Merhabalar,
 
+1.test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+CREATE TABLE employee(
+	id INTEGER,
+	name VARCHAR(50),
+	birthday DATE,
+	email VARCHAR(100)
+);
+2.Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+3.Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+UPDATE employee 
+SET name = 'ali', 	 
+    birthday = '2022-01-01',
+    email = 'ali@gmail.com' 
+	WHERE id = 7
+	RETURNING *;
+  .
+  .
+  .
+4.Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+DELETE FROM employee
+WHERE name = 'Kristoforo';
+.
+.
+.
+.
